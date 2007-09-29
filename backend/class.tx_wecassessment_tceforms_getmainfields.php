ceae34a8371cabcb0c1ed969c8903f5adabcb19d
Jeff Segars <jeff@webempoweredchurch.org>
2007-09-29 03:10:46 +0000

    Added hook to set new response data based on previous response.

--- backend/class.tx_wecassessment_tceforms_getmainfields.php
+++ backend/class.tx_wecassessment_tceforms_getmainfields.php
@@ -17,15 +17,17 @@ class tx_wecassessment_tceforms_getmainfields {
 		
 		if($table == 'tx_wecassessment_response') {
 			/* If we have posted data and a new record, preset values to what they were on the previous record */
-			/*
 			if(is_array($GLOBALS['HTTP_POST_VARS']['data']['tx_wecassessment_response']) && strstr($row['uid'], 'NEW')) {
 				$postData = array_pop($GLOBALS['HTTP_POST_VARS']['data']['tx_wecassessment_response']);
+				
+				$row['type'] = $postData['type'];
+				$row['question_id'] = $postData['question_id'];
+				$row['category_id'] = $postData['category_id'];
 				
 				$range = $postData['max_value'] - $postData['min_value'];
 				$row['min_value'] = $postData['max_value'];
 				$row['max_value'] = $postData['max_value'] + $range;
 			}
-			*/
 		}
 	}
 }
